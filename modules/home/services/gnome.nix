e52c79d9e1df1e8d5f8d69bc829f9528902b601d
crative <hannig.sebi@gmail.com>
2024-07-16 12:58:57 +0200

    gtk themes and env variable

--- modules/home/services/gnome.nix
+++ modules/home/services/gnome.nix
@@ -1,7 +1,37 @@
 { config, pkgs, ... }:
 
 {
+  gtk = {
+    enable = true;
+    theme = {
+      name = "Breeze-Dark"; 
+      package = pkgs.libsForQt5.breeze-gtk;
+    };
+    iconTheme = {
+      name = "Papirus-Dark";
+      package = pkgs.catppuccin-papirus-folders.override {
+        flavor = "mocha";
+        accent = "lavender";
+      };
+    };
+    gtk3 = {
+      extraConfig.gtk-application-prefer-dark-theme = true;
+    };
+  };
+
+  home.pointerCursor = {
+    gtk.enable = true;
+    name = "Catppuccin-Mocha-Light-Cursors";
+    package = pkgs.catppuccin-cursors.mochaLight;
+    size = 16;
+  };
+
   dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      gtk-theme = "Breeze-Dark";
+      color-scheme = "prefer-dark";
+    };
+
     "org/gnome/shell/app-switcher" = {
       current-workspace-only = true;
     };
