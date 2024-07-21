8969b9d690324639e1fae1c08e0806935f80e4fd
crative <hannig.sebi@gmail.com>
2024-07-21 21:26:48 +0200

    rebuildh

--- modules/home/services/sh.nix
+++ modules/home/services/sh.nix
@@ -5,6 +5,7 @@ let
     # Nix stuff
     nixconfig = "cd ~/.dotfiles";
     rebuild = "ga . && sudo nixos-rebuild switch --flake ~/.dotfiles/ && hrebuild";
+    rebuildh = "ga . && sudo nixos-rebuild switch --flake ~/.dotfiles/";
     hrebuild = "ga . && home-manager switch --flake ~/.dotfiles/";
     nixupdate = "cd ~/.dotfiles && sudo nix flake update";
     nix-clean = "sudo nix-collect-garbage && sudo nix-collect-garbage -d && nix-collect-garbage && nix-collect-garbage -d";
