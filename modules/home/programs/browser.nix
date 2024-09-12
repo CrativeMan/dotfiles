60c1da7de9f330b85c71b04920910294584bce4e
crative <hannig.sebi@gmail.com>
2024-09-12 14:18:55 +0200

    firefox fix

--- modules/home/programs/browser.nix
+++ modules/home/programs/browser.nix
@@ -5,25 +5,25 @@
     programs.firefox = {
       enable = true;
       #nativeMessagingHosts = [pkgs.gnomeExtensions.gsconnect];
-      profiles.default = {
-        name = "Default";
-        settings = {
-          "browser.tabs.loadInBackground" = true;
-          "widget.gtk.rounded-bottom-corners.enabled" = true;
-          "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
-          "svg.context-properties.content.enabled" = true;
-          "gnomeTheme.hideSingleTab" = false;
-          "gnomeTheme.normalWidthTabs" = false;
-          "gnomeTheme.tabsAsHeaderbar" = false;
-          #"browser.toolbars.bookmarks.visibility" = "never";
-        };
-        userChrome = ''
-          @import "firefox-gnome-theme/userChrome.css";
-        '';
-        userContent = ''
-          @import "firefox-gnome-theme/userContent.css";
-        '';
-      };
+      # profiles.default = {
+      #   name = "Default";
+      #   settings = {
+      #     "browser.tabs.loadInBackground" = true;
+      #     "widget.gtk.rounded-bottom-corners.enabled" = true;
+      #     "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+      #     "svg.context-properties.content.enabled" = true;
+      #     "gnomeTheme.hideSingleTab" = false;
+      #     "gnomeTheme.normalWidthTabs" = false;
+      #     "gnomeTheme.tabsAsHeaderbar" = false;
+      #     #"browser.toolbars.bookmarks.visibility" = "never";
+      #   };
+      #   userChrome = ''
+      #     @import "firefox-gnome-theme/userChrome.css";
+      #   '';
+      #   userContent = ''
+      #     @import "firefox-gnome-theme/userContent.css";
+      #   '';
+      # };
     };
 
     programs.chromium = {
