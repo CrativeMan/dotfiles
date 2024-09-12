70a8eaa70a177a7e8e329e10261d703a78e9a8c5
crative <hannig.sebi@gmail.com>
2024-09-12 13:56:36 +0200

    kitty chagnes

--- modules/home/programs/kitty.nix
+++ modules/home/programs/kitty.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ...}:
+{ config, pkgs, ... }:
 
 {
   programs.kitty = {
@@ -6,7 +6,7 @@
     settings = {
       confirm_os_window_close = 0;
       #background = "#1c1b22";
-      background_opacity = "0.95";
+      background_opacity = "0.8";
       background_blur = 1;
     };
     #theme = "Duotone Dark";
