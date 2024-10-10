8622dc4cb8c382f0e7b337fc091fb1847881dd41
crative <hannig.sebi@gmail.com>
2024-10-10 16:06:19 +0200

    iasd:w

--- modules/home/services/gnome.nix
+++ modules/home/services/gnome.nix
@@ -72,8 +72,7 @@
       picture-options = "zoom";
       #picture-uri = "file:///home/crative/.dotfiles/wallpapers/wallhaven-yx6e9l_3840x2160.png";
       picture-uri = "file:///home/crative/.dotfiles/wallpapers/a_white_light_pole_with_a_jet_trail_in_the_sky.jpg";
-      picture-uri-dark = "file:///home/crative/.dotfiles/wallpapers/woman_balcony.jpeg";
+      picture-uri-dark = "file:///home/crative/.dotfiles/wallpapers/wallhaven-yx6e9l_3840x2160.png";
     };
-
   };
 }
