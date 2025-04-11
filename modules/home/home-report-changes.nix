f973125889377296aa630514f2a39256b74c377c
Sebastian Hannig <hannig.sebi@gmail.com>
2025-04-11 08:14:25 +0200

    snytax

--- modules/home/home-report-changes.nix
+++ modules/home/home-report-changes.nix
@@ -1,7 +1,6 @@
-{ config
-, pkgs
-, ...
-}: {
+{ config, pkgs, ... }:
+
+{
   home.activation.profile-report-changes = config.lib.dag.entryAnywhere ''
     ${pkgs.nvd}/bin/nvd --nix-bin-dir=${pkgs.nix}/bin diff $oldGenPath $newGenPath
   '';
