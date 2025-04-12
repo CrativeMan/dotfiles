c6207b0cff893173122b007c6252a267c3e41c45
Sebastian Hannig <hannig.sebi@gmail.com>
2025-04-12 22:46:34 +0200

    planify quick add desktop entry, icon not working

--- modules/home/config/desktop-entries.nix
+++ modules/home/config/desktop-entries.nix
@@ -28,6 +28,13 @@
                 terminal = true;
                 categories = [ "Development" "X-Custom" ];
             };
+            "planify-quick-add" = {
+                name = "Add Todo";
+                genericName = "Planify Quick Add";
+                exec = "io.github.alainm23.planify.quick-add";
+                icon = "${vars.self}/assets/icons/todoist.svg";
+                terminal = false;
+            };
         };
     };
 }
