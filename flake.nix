48c87dca089969ca57821366c998d5547000757f
CrativeMan <hannig.sebi@gmail.com>
2025-04-19 23:33:00 +0200

    format

--- flake.nix
+++ flake.nix
@@ -49,53 +49,56 @@
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, nixos-hardware, ... }@inputs:
-    let
-      lib = nixpkgs.lib;
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-      vars = {
-        user = "crative";
-        gitUser = "CrativeMan";
-        email = "hannig.sebi@gmail.com";
-        editor = "nvim";
-        flakeDir = "/home/${vars.user}/.dotfiles";
-        self = self;
-      };
-    in
-    {
-      nixosConfigurations = {
-        nixos = lib.nixosSystem {
-          specialArgs = {
-            inherit inputs vars;
-            host = {
-              hostName = "nixos";
-            };
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    nixos-hardware,
+    ...
+  } @ inputs: let
+    lib = nixpkgs.lib;
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+    vars = {
+      user = "crative";
+      gitUser = "CrativeMan";
+      email = "hannig.sebi@gmail.com";
+      editor = "nvim";
+      flakeDir = "/home/${vars.user}/.dotfiles";
+      self = self;
+    };
+  in {
+    nixosConfigurations = {
+      nixos = lib.nixosSystem {
+        specialArgs = {
+          inherit inputs vars;
+          host = {
+            hostName = "nixos";
           };
-          inherit system;
-          modules = [ ./modules/nix/configuration.nix ./hosts/nixos-desktop/default.nix ];
         };
-        framework = lib.nixosSystem {
-          specialArgs = {
-            inherit inputs vars nixos-hardware;
-            host = {
-              hostName = "framework";
-            };
-          };
-          inherit system;
-          modules = [ ./modules/nix/configuration.nix ./hosts/nixos-framework/default.nix ];
-        };
+        inherit system;
+        modules = [./modules/nix/configuration.nix ./hosts/nixos-desktop/default.nix];
       };
-
-      homeConfigurations = {
-        crative = home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
-
-          extraSpecialArgs = { inherit inputs vars; };
-
-          modules = [ ./modules/home/home.nix ];
+      framework = lib.nixosSystem {
+        specialArgs = {
+          inherit inputs vars nixos-hardware;
+          host = {
+            hostName = "framework";
+          };
         };
+        inherit system;
+        modules = [./modules/nix/configuration.nix ./hosts/nixos-framework/default.nix];
       };
     };
 
+    homeConfigurations = {
+      crative = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        extraSpecialArgs = {inherit inputs vars;};
+
+        modules = [./modules/home/home.nix];
+      };
+    };
+  };
 }
