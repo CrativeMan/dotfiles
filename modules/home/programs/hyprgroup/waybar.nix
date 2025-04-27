7be41cf85deb66b5445b25ba0429068dbf258d90
CrativeMan <hannig.sebi@gmail.com>
2025-04-27 18:09:55 +0200

    yayy waybar

--- modules/home/programs/hyprgroup/waybar.nix
+++ modules/home/programs/hyprgroup/waybar.nix
@@ -16,7 +16,7 @@
       mainBar = {
         layer = "top";
         position = "top";
-        height = 35;
+        height = 30;
         spacing = 7;
 
         output = ["*"];
@@ -102,7 +102,7 @@
           tooltip = false;
         };
 
-        "custom-shutdown" = {
+        "custom/shutdown" = {
           format = "⏻";
           tooltip = "Shutdown";
           on-click = "/home/${vars.user}/.config/waybar/scripts/shutdown_prompt.sh";
@@ -112,66 +112,61 @@
     };
 
     style = ''
-      @define-color rosewater #f5e0dc;
-      @define-color flamingo #f2cdcd;
-      @define-color pink #f5c2e7;
-      @define-color mauve #cba6f7;
-      @define-color red #f38ba8;
-      @define-color maroon #eba0ac;
-      @define-color peach #fab387;
-      @define-color yellow #f9e2af;
-      @define-color green #a6e3a1;
-      @define-color teal #94e2d5;
-      @define-color sky #89dceb;
-      @define-color sapphire #74c7ec;
-      @define-color blue #89b4fa;
-      @define-color lavender #b4befe;
-      @define-color text #cdd6f4;
-      @define-color subtext1 #bac2de;
-      @define-color subtext0 #a6adc8;
-      @define-color overlay2 #9399b2;
-      @define-color overlay1 #7f849c;
-      @define-color overlay0 #6c7086;
-      @define-color surface2 #585b70;
-      @define-color surface1 #45475a;
-      @define-color surface0 #313244;
-      @define-color base #1e1e2e;
-      @define-color mantle #181825;
-      @define-color crust #11111b;
+        @define-color rosewater #f5e0dc;
+        @define-color flamingo #f2cdcd;
+        @define-color pink #f5c2e7;
+        @define-color mauve #cba6f7;
+        @define-color red #f38ba8;
+        @define-color maroon #eba0ac;
+        @define-color peach #fab387;
+        @define-color yellow #f9e2af;
+        @define-color green #a6e3a1;
+        @define-color teal #94e2d5;
+        @define-color sky #89dceb;
+        @define-color sapphire #74c7ec;
+        @define-color blue #89b4fa;
+        @define-color lavender #b4befe;
+        @define-color text #cdd6f4;
+        @define-color subtext1 #bac2de;
+        @define-color subtext0 #a6adc8;
+        @define-color overlay2 #9399b2;
+        @define-color overlay1 #7f849c;
+        @define-color overlay0 #6c7086;
+        @define-color surface2 #585b70;
+        @define-color surface1 #45475a;
+        @define-color surface0 #313244;
+        @define-color base #1e1e2e;
+        @define-color mantle #181825;
+        @define-color crust #11111b;
 
-      * {
-        font-family: FantasqueSansMono Nerd Font;
-        font-size: 17px;
-        min-height: 0;
+        * {
+          font-family: FantasqueSansMono Nerd Font;
+          font-size: 14px;
+          min-height: 0;
       }
 
       #waybar {
-        background-color: @surface0;
-        color: @text;
-        margin: 5px 5px;
+          background-color: @surface0;
+          color: @text;
+          margin: 5px 5px;
       }
 
       #workspaces {
-        border-radius: 1rem;
-        margin: 5px;
-        background-color: @surface0;
-        margin-left: 1rem;
+          border-radius: 1rem;
+          margin: 5px;
+          background-color: @surface0;
+          margin-left: 1rem;
       }
 
       #workspaces button {
-        color: @lavender;
-        border-radius: 1rem;
-        padding: 0.4rem;
+          color: @lavender;
+          border-radius: 1rem;
+          padding: 0.4rem;
       }
 
       #workspaces button.active {
-        color: @sky;
-        border-radius: 1rem;
-      }
-
-      #workspaces button:hover {
-        color: @sapphire;
-        border-radius: 1rem;
+          color: @sky;
+          border-radius: 1rem;
       }
 
       #tray,
@@ -182,34 +177,35 @@
       #clock,
       #battery,
       #pulseaudio,
+      #custom-launcher,
       #custom-shutdown {
-        background-color: @surface0;
-        padding: 0.5rem 1rem;
-        margin: 5px 0;
+          background-color: @surface0;
+          padding: 0.5rem 1rem;
+          margin: 5px 0;
       }
 
       #clock {
-        color: @blue;
-        border-radius: 0px 1rem 1rem 0px;
-        margin-right: 1rem;
+          color: @blue;
+          border-radius: 0px 1rem 1rem 0px;
+          margin-right: 1rem;
       }
 
       #battery {
-        color: @green;
+          color: @green;
       }
 
       #battery.charging {
-        color: @green;
+          color: @green;
       }
 
       #battery.warning:not(.charging) {
-        color: @red;
+          color: @red;
       }
 
       #pulseaudio {
-        color: @maroon;
-        border-radius: 1rem 0px 0px 1rem;
-        margin-left: 1rem;
+          color: @maroon;
+          border-radius: 1rem 0px 0px 1rem;
+          margin-left: 1rem;
       }
 
       #custom-shutdown {
@@ -219,8 +215,8 @@
       }
 
       #tray {
-        margin-right: 1rem;
-        border-radius: 1rem;
+          margin-right: 1rem;
+          border-radius: 1rem;
       }
     '';
   };
