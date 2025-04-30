baac61031fd59bda10823b6b68ded203351f51a8
Sebastian Hannig <hannig.sebi@gmail.com>
2025-04-30 11:08:36 +0200

    script proj v1.0.0

--- modules/nix/scripts/scripts.nix
+++ modules/nix/scripts/scripts.nix
@@ -13,11 +13,8 @@
     runtimeInputs = [pkgs.gum pkgs.jq];
     text = ''
       set +o nounset
+
       url=$1
-      if [ -z "$url" ]; then
-        echo "Usage: $0 <flake-url>"
-        exit 1
-      fi
 
       if ! command -v gum >/dev/null 2>&1; then
         echo "gum is not installed. Please install it and ensure it's in your PATH."
@@ -29,6 +26,15 @@
       else
         echo "jq is not available in the environment, using nix-shell"
         jq_cmd="nix run nixpkgs#jq"
+      fi
+
+      known_urls=(
+          "git+ssh://git@github.com/CrativeMan/dotfiles"
+          "git+ssh://git@github.com/ewuuwe/dotfiles"
+      )
+
+      if [ -z "$url" ]; then
+          url=$(printf "%s\n" "''${known_urls[@]}" | gum choose)
       fi
 
       options=$($jq_cmd -r '.templates | keys[]' <(nix flake show "$url" --json))
