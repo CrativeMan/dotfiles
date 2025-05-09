0b5a0c0e338fb4cdece38c50faa1c332a330db14
Sebastian Hannig <hannig.sebi@gmail.com>
2025-05-09 13:45:28 +0200

    golang flake

---
+++ templates/golang/flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+  outputs = inputs @ {
+    flake-parts,
+    systems,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = import systems;
+      perSystem = {pkgs, ...}: {
+        formatter = pkgs.alejandra;
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.go_1_24
+            pkgs.gotools
+            pkgs.golangci-lint
+          ];
+        };
+      };
+    };
+}
