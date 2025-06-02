072bc23e9991f2ceb7cb4746f0a9c6d7f11c1202
CrativeMan <hannig.sebi@gmail.com>
2025-06-02 20:29:19 +0200

    update, rename because of update

--- modules/nix/configuration.nix
+++ modules/nix/configuration.nix
@@ -1,8 +1,4 @@
-{
-  pkgs,
-  lib,
-  ...
-}: {
+{pkgs, ...}: {
   imports = [
     ./programs/default.nix
     ./services/default.nix
@@ -84,8 +80,8 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
   services.xserver = {
